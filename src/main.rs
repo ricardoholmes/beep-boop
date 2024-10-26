97d0dd34b07b215916b2f01b932961ab1a8bb01a
ricardoholmes <ricardoholmes2004@gmail.com>
2024-10-26 12:52:18 +0100

    Create temperature bar chart

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,10 @@ use std::io;
 
 use ratatui::{
     crossterm::event::{self, KeyCode, KeyEventKind},
-    style::Stylize,
-    widgets::Paragraph,
-    DefaultTerminal,
+    style::{Color, Style, Stylize},
+    text::Line,
+    widgets::{Bar, BarChart, BarGroup, Block, Paragraph},
+    DefaultTerminal
 };
 
 fn main() -> io::Result<()> {
@@ -18,10 +19,13 @@ fn main() -> io::Result<()> {
 fn run(mut terminal: DefaultTerminal) -> io::Result<()> {
     loop {
         terminal.draw(|frame| {
-            let greeting = Paragraph::new("Hello Ratatui! (press 'q' to quit)")
-                .white()
-                .on_blue();
-            frame.render_widget(greeting, frame.area());
+            let exit_prompt = Paragraph::new("Hi! Press 'q' to quit");
+            frame.render_widget(exit_prompt, frame.area());
+
+            let temperatures = [86,76,90,71,78,79,85,53,73,50,63,84,66,69,53,72,61,78,75,67];
+            let bar_chart = vertical_barchart(&temperatures);
+
+            frame.render_widget(bar_chart, frame.area());
         })?;
 
         if let event::Event::Key(key) = event::read()? {
@@ -30,4 +34,34 @@ fn run(mut terminal: DefaultTerminal) -> io::Result<()> {
             }
         }
     }
+}
+
+fn vertical_barchart(temperatures: &[u8]) -> BarChart {
+    let bars: Vec<Bar> = temperatures
+        .iter()
+        .enumerate()
+        .map(|(hour, value)| vertical_bar(hour, value))
+        .collect();
+
+    let title = Line::from("Weather (Vertical)").centered();
+    BarChart::default()
+        .data(BarGroup::default().bars(&bars))
+        .block(Block::new().title(title))
+        .bar_width(5)
+}
+
+fn vertical_bar(hour: usize, temperature: &u8) -> Bar {
+    Bar::default()
+        .value(u64::from(*temperature))
+        .label(Line::from(format!("{hour:>02}:00")))
+        .text_value(format!("{temperature:>3}°"))
+        .style(temperature_style(*temperature))
+        .value_style(temperature_style(*temperature).reversed())
+}
+
+/// create a yellow to red value based on the value (50-90)
+fn temperature_style(value: u8) -> Style {
+    let green = (255.0 * (1.0 - f64::from(value - 50) / 40.0)) as u8;
+    let color = Color::Rgb(255, green, 0);
+    Style::new().fg(color)
 }
